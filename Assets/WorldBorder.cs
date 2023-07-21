4814fb87c80f58e3095dc764f951d19b1b1b62c7
VietTung0801 <tunle0801@gmail.com>
2023-07-21 11:41:16 +0700

    UI update + worldBorder update

--- Assets/WorldBorder.cs
+++ Assets/WorldBorder.cs
@@ -5,11 +5,7 @@ public class WorldBorder : MonoBehaviour
 {
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if (collision.TryGetComponent(out PlayerController player))
-        {
-            PopUpManager.Instance.SpawnText("Do not fly out of border", player.transform.position, Color.white);
-            return;
-        }
+        if (!collision.CompareTag("Bullet")) return;
         if (CinemachineManager.Instance.VCam.m_Follow == collision.transform)
         {
             CinemachineManager.Instance.SetFollow(null);
