c371c8abf3cae40bec0c3eaeee85ee4f6c384f01
VietTung0801 <tunle0801@gmail.com>
2023-07-23 16:35:47 +0700

    GameOverScreen fade in (using CanvasGroup)

--- Assets/_Scripts/GameOverScreen.cs
+++ Assets/_Scripts/GameOverScreen.cs
@@ -1,16 +1,21 @@
-using System.Collections;
-using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 
+[RequireComponent(typeof(CanvasGroup))]
 public class GameOverScreen : MonoBehaviour
 {
+    private CanvasGroup _canvasGroup;
     private void Awake()
     {
         gameObject.SetActive(false);
+        _canvasGroup = GetComponent<CanvasGroup>();
+        _canvasGroup.alpha = 0f;
     }
 
     public void Show()
     {
+        gameObject.SetActive(true);
+        DOTween.To(() => _canvasGroup.alpha, x => _canvasGroup.alpha = x, endValue: 1, duration: 1);
         foreach (Team team in DataPersistence.GetOpenedTeams())
         {
             if (DataPersistence.Get(team).isDestroyed) continue;
