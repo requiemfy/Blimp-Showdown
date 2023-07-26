e51a2bed5b46a2bebcc1e6ccb188cf944d87a532
VietTung0801 <tunle0801@gmail.com>
2023-07-26 21:56:42 +0700

    endTurnConfirm update + scaleWithZoom unsubscribe on disable

--- Assets/_Scripts/ScaleWithZoom.cs
+++ Assets/_Scripts/ScaleWithZoom.cs
@@ -4,10 +4,16 @@ public class ScaleWithZoom : MonoBehaviour
 {
     private void Start()
     {
-        CameraManager.Instance.WhileZoomChanging += (float orthographicSize) =>
-        {
-            float size = (float)orthographicSize / 5;
-            transform.localScale = new Vector2(size, size);
-        };
+        CameraManager.Instance.WhileZoomChanging += Scale;
+    }
+    private void OnDisable()
+    {
+        CameraManager.Instance.WhileZoomChanging -= Scale;
+    }
+
+    private void Scale(float orthographicSize)
+    {
+        float size = (float)orthographicSize / 5;
+        transform.localScale = new Vector2(size, size);
     }
 }
