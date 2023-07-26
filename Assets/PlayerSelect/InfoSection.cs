506e5030022118ffd2d29f636ac68f2a73cd2b85
VietTung0801 <tunle0801@gmail.com>
2023-07-26 14:14:49 +0700

    InfoSection fully added

--- Assets/PlayerSelect/InfoSection.cs
+++ Assets/PlayerSelect/InfoSection.cs
@@ -4,29 +4,45 @@ using UnityEngine.UI;
 
 public class InfoSection : MonoBehaviour
 {
-    [Header("Text")]
-    [SerializeField] private TextMeshProUGUI nameTMP;
-    [SerializeField] private TextMeshProUGUI energyTMP;
-    [SerializeField] private TextMeshProUGUI healthTMP;
-    [SerializeField] private TextMeshProUGUI damageTMP;
-    [SerializeField] private TextMeshProUGUI bulletCountTMP;
+    [Header("Header")]
+    [SerializeField] TextMeshProUGUI nameTMP;
+    [SerializeField] TextMeshProUGUI energyTMP;
+    [SerializeField] Image thumbnail;
+
+    [Header("Body")]
+    [SerializeField] TextMeshProUGUI healthTMP;
+    [SerializeField] TextMeshProUGUI damageTMP;
+    [SerializeField] TextMeshProUGUI rangeTMP;
+    [SerializeField] TextMeshProUGUI explodeRadiusTMP;
+    [SerializeField] TextMeshProUGUI bulletCountTMP;
+    [SerializeField] TextMeshProUGUI trajectoryTMP;
 
     [Header("Button")]
     [SerializeField] private Button closeBtn;
 
     private void Start()
     {
+        gameObject.SetActive(false);
         closeBtn.onClick.AddListener(() => { gameObject.SetActive(false); });
     }
 
     public void ShowInfoOf(WeaponType weapon)
     {
         gameObject.SetActive(true);
+
         //flip card animation
+
+        //header
         nameTMP.text = weapon.name;
         energyTMP.text = $"{weapon.energyCost}";
+        thumbnail.sprite = weapon.barrel;
+
+        //body
         healthTMP.text = $"{weapon.health}";
         damageTMP.text = $"{weapon.damage}";
+        rangeTMP.text = $"{weapon.range}";
+        explodeRadiusTMP.text = $"{weapon.explodeRadius}";
         bulletCountTMP.text = $"x{weapon.bulletCount}";
+        trajectoryTMP.text = weapon.isGravityAffected ? "parabola" : "straight line";
     }
 }
