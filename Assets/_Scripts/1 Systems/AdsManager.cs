514c0b6e69861dc69cfcc68bf43ce5348b080bab
VietTung0801 <tunle0801@gmail.com>
2023-08-07 16:42:38 +0700

    Google admob package added

---
+++ Assets/_Scripts/1 Systems/AdsManager.cs
@@ -0,0 +1,72 @@
+using UnityEngine;
+using GoogleMobileAds.Api;
+
+public class AdsManager : MonoBehaviour
+{
+    public static AdsManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    private void Start()
+    {
+        MobileAds.Initialize((InitializationStatus initStatus) =>
+        {
+            Debug.Log("Ads ready");
+        });
+    }
+
+    // These ad units are configured to always serve test ads.
+#if UNITY_ANDROID
+    private string _adUnitId = "ca-app-pub-3940256099942544/1033173712";
+#elif UNITY_IPHONE
+  private string _adUnitId = "ca-app-pub-3940256099942544/4411468910";
+#else
+  private string _adUnitId = "unused";
+#endif
+
+    private InterstitialAd interstitialAd;
+
+    public void LoadInterstitialAd()
+    {
+        // Clean up the old ad before loading a new one.
+        if (interstitialAd != null)
+        {
+            interstitialAd.Destroy();
+            interstitialAd = null;
+        }
+
+        Debug.Log("Loading the interstitial ad.");
+
+        // create our request used to load the ad.
+        var adRequest = new AdRequest();
+        adRequest.Keywords.Add("unity-admob-sample");
+
+        // send the request to load the ad.
+        InterstitialAd.Load(_adUnitId, adRequest,
+            (InterstitialAd ad, LoadAdError error) =>
+            {
+                // if error is not null, the load request failed.
+                if (error != null || ad == null)
+                {
+                    Debug.LogError("interstitial ad failed to load an ad " +
+                                   "with error : " + error);
+                    return;
+                }
+
+                Debug.Log("Interstitial ad loaded with response : "
+                          + ad.GetResponseInfo());
+
+                interstitialAd = ad;
+            });
+    }
+}
