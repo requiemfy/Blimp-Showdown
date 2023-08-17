e052836c655814041d3fcb226c4e42ffd81b94bf
VietTung0801 <tunle0801@gmail.com>
2023-08-17 21:39:06 +0700

    safezone editor tool added

--- Assets/_Scripts/BorderShrinking.cs
+++ Assets/_Scripts/BorderShrinking.cs
@@ -5,6 +5,15 @@ public class BorderShrinking : MonoBehaviour
 {
     [SerializeField]
     private float unitScale = 2.51f;
+    [SerializeField]
+    private Vector2Int pos;
+    [SerializeField]
+    private Vector2Int size;
+    private void OnValidate()
+    {
+        transform.position = new Vector2((pos.x + 0.5f) * unitScale, (pos.y + 0.5f) * unitScale);
+        transform.localScale = new Vector2(size.x * unitScale, size.y * unitScale);
+    }
     private void Awake()
     {
         GameManager.Instance.onCycleEnded += () =>
